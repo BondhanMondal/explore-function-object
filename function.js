6161198e7f9f5e161cdcc6b25075b76db6e683be
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-30 01:47:25 +0600

    function added

---
+++ function.js
@@ -0,0 +1,21 @@
+// function declaration 
+
+function startFan(){
+    console.log('work towards the switch');
+    console.log('press the switch');
+}
+
+// call the function 
+startFan();
+
+console.log('eat breakfast');
+console.log('Drink tea');
+console.log('take a shower');
+
+startFan();
+startFan();
+
+var country = "bd";
+var city = 'Dhaka';
+var location = 'Gulisthan';
+startFan();
